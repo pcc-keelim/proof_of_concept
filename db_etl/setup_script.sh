e6629b395ceb938bcd6709d9651baf9846dab756
Marc Keeling <marc.keeling@dc2-ds-db-01.int.collectivemedicaltech.com>
2023-06-28 09:53:11 -0600

    changed db locations and file permissions

--- db_etl/setup_script.sh
+++ db_etl/setup_script.sh
@@ -1,8 +1,8 @@
 sudo rm -rf ./clickhouse_logs
 # Remove for productsion
-sudo rm -rf ./clickhouse_database
+sudo rm -rf /data/ch_db
 # Remove for productsion
-mkdir ./clickhouse_database
+sudo mkdir /data/ch_db
 
 # Create flags file if not exists
 file=flags.yml
@@ -10,7 +10,8 @@ if [ ! -e "$file" ] ; then
     touch "$file"
 fi
 
-sudo chown clickhouse:clickhouse clickhouse_database/
+sudo chown clickhouse:clickhouse /data/ch_db/
+sudo chmod -R 770 /data/ch_db/
 sudo chown clickhouse:clickhouse clickhouse-server/
-sudo chmod -R 775 ./clickhouse-server/
-docker compose up --force-recreate --no-cache
+sudo chmod -R 770 ./clickhouse-server/
+docker compose -f docker-compose-db-etl.yml up -d
