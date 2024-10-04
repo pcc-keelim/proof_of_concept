322c558822c2204d7edc5bb122e45439b8d30797
julianjurkoiccmt <105886080+julianjurkoiccmt@users.noreply.github.com>
2024-10-04 17:03:33 -0600

    adding sendable folder, need to rethink how it's sent

--- setup.sh
+++ setup.sh
@@ -6,9 +6,16 @@
 # replace any value following "ca_cert:" with "/windows_shared/.ssl/CMT-CA-CHAIN.crt"
 sed -i 's/ca_cert: .*/ca_cert: \/root\/.ssl\/CMT-CA-CHAIN.crt/' /windows_shared/.dbt/profiles.yml
 
+# modify the contents of windows_shared/secrets/secrets.yaml
+# replace any value following "ca_cert:" with "/windows_shared/.ssl/CMT-CA-CHAIN.crt"
+sed -i 's/ca_cert: .*/ca_cert: \/root\/.ssl\/CMT-CA-CHAIN.crt/' /windows_shared/secrets/secrets.yaml
+
 # copy the contents of /windows_shared to /root
-cp -r /windows_shared/.* /root
+cp -r /windows_shared/* /root
 chmod -R 700 /root/.ssh
+
+# copy pem file to etc/ssl/certs
+cp /root/.ssl/CMT_Root_CA.pem /etc/ssl/certs
 
 # prompt git config email and name
 echo "Enter your git user name (ie john.doe):"
@@ -26,5 +33,4 @@ git config --global --add safe.directory /code/ds-reporting-scheduling
 # run dbt setup
 cd  /code/ds-reporting-logic/src/dbt_datascience
 dbt debug 
-dbt deps
 dbt compile
