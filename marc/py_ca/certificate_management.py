a2267136b1fef54b71a17a54295334fcec547c9a
Marc Keeling <marc.keeling@pointclickcare.com>
2024-10-09 06:34:45 -0600

    added import

--- marc/py_ca/certificate_management.py
+++ marc/py_ca/certificate_management.py
@@ -5,7 +5,7 @@ from cryptography import x509
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.x509.oid import NameOID
-from getpass import getpass
+import sys
 
 class CertificateBase:
     """
